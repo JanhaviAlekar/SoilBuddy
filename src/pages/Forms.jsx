7de30c408b5da4259ad1429a94925806f3a0c0d4
Aryan <aryan1bhokare@gmail.com>
2023-04-21 23:30:32 +0530

    Modified Form

--- src/pages/Forms.jsx
+++ src/pages/Forms.jsx
@@ -3,7 +3,7 @@ import Form from '../components/Form'
 
 const Forms = () => {
   return (
-    <div className='min-h-screen'>
+    <div className='flex justify-center h-[80vh] p-20 min-w-full' >
         <Form/>
     </div>
   )
